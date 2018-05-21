408c7df44c33b5777be55e5ad7206c4ef1a7eb26
nad407 <37850036+nad407@users.noreply.github.com>
2018-05-21 17:25:51 +0200

    6kyu kata

---
+++ IQTest/IQ.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IQTest
+{
+    class IQ
+    {
+        static int even;
+        static int odd;
+        static int posEven;
+        static int posOdd;
+
+        public static int Test(string numbers)
+        {
+            string[] nums = numbers.Split(null);
+
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                int num = Int32.Parse(nums[i-1]);
+                ;
+                if (num % 2 == 0)
+                {
+                    even++;
+                    posEven = i;
+                }
+                else
+                {
+                    odd++;
+                    posOdd = i;
+                }
+            }
+            
+            if (even == 1) { return posEven; }
+            else if (odd == 1) { return posOdd; }
+            else return 0;
+
+        }
+    }
+}
---
+++ IQTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans = IQ.Test("1 2 2");
+            Console.WriteLine(ans);
+
+            Console.ReadKey();
+        }
+    }
+}
